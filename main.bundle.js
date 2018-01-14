2f7763991159396883604144d4e8c1b07de62035
Zachary Toney <zach@clearseries.com>
2018-01-14 18:28:58 -0500

    Added Resume button

--- main.bundle.js
+++ main.bundle.js
@@ -38,7 +38,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<app-navbar></app-navbar>\r\n<app-jumbotron></app-jumbotron>\r\n<app-projects-home></app-projects-home>"
+module.exports = "<app-navbar></app-navbar>\r\n<app-jumbotron></app-jumbotron>\r\n<app-skills-home></app-skills-home>"
 
 /***/ }),
 
@@ -84,12 +84,14 @@ var AppComponent = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_navbar_navbar_component__ = __webpack_require__("../../../../../src/app/components/navbar/navbar.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__components_jumbotron_jumbotron_component__ = __webpack_require__("../../../../../src/app/components/jumbotron/jumbotron.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__components_projects_home_projects_home_component__ = __webpack_require__("../../../../../src/app/components/projects-home/projects-home.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__components_skills_home_skills_home_component__ = __webpack_require__("../../../../../src/app/components/skills-home/skills-home.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -106,6 +108,7 @@ var AppModule = /** @class */ (function () {
                 __WEBPACK_IMPORTED_MODULE_3__components_navbar_navbar_component__["a" /* NavbarComponent */],
                 __WEBPACK_IMPORTED_MODULE_4__components_jumbotron_jumbotron_component__["a" /* JumbotronComponent */],
                 __WEBPACK_IMPORTED_MODULE_5__components_projects_home_projects_home_component__["a" /* ProjectsHomeComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__components_skills_home_skills_home_component__["a" /* SkillsHomeComponent */],
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
@@ -129,7 +132,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "/*Icon styling*/\r\n.tagline-icons{\r\n\tdisplay: inline;\r\n}\r\n.icon-div{\r\n\tdisplay: inline;\r\n\tvertical-align: bottom;\r\n}\r\n.angular-icon{\r\n\tcolor: #DC0330;\r\n\t-webkit-transition: all 0.5s ease 0s;\r\n\ttransition: all 0.5s ease 0s;\r\n}\r\n.angular-icon:hover{\r\n\tcolor: #212529;\r\n}\r\n.html5-icon{\r\n\tcolor: #F57933;\r\n\t-webkit-transition: all 0.5s ease 0s;\r\n\ttransition: all 0.5s ease 0s;\r\n}\r\n.html5-icon:hover{\r\n\tcolor: #212529;\r\n}\r\n.css3-icon{\r\n\tcolor: #018ED5;\r\n\t-webkit-transition: all 0.5s ease 0s;\r\n\ttransition: all 0.5s ease 0s;\r\n}\r\n.css3-icon:hover{\r\n\tcolor: #212529;\r\n}\r\n.arrow-down-icon{\r\n\tpadding-top: 1em;\r\n\tfont-size: 3em;\r\n  position:relative;\r\n  -webkit-animation:bounce 1s infinite;\r\n}\r\n@-webkit-keyframes bounce {\r\n  0%       { bottom:5px; }\r\n  25%, 75% { bottom:15px; }\r\n  50%      { bottom:20px; }\r\n  100%     {bottom:0;}\r\n}\r\n/*END Icon styling*/\r\n/*Jumbotron background styling*/\r\n.jumbotron {\r\n/*background-image: url(\"/assets/Homepage/jumbotron-home-background-texture.png\");*/\r\nbackground-color: white;\r\nbackground-size: cover;\r\nborder-radius: 0px;\r\n}\r\n/*END Jumbotron backgrund styling*/\r\n*{\r\n\tfont-family: Maison Neue,Helvetica Neue,Helvetica,Arial,sans-serif;\r\n}\r\n\r\n", ""]);
+exports.push([module.i, "/*Icon styling*/\r\n.tagline-icons{\r\n\tdisplay: inline;\r\n}\r\n.icon-div{\r\n\tdisplay: inline;\r\n\tvertical-align: bottom;\r\n}\r\n.angular-icon{\r\n\tcolor: #DC0330;\r\n\t-webkit-transition: all 0.5s ease 0s;\r\n\ttransition: all 0.5s ease 0s;\r\n}\r\n.angular-icon:hover{\r\n\tcolor: #212529;\r\n}\r\n.html5-icon{\r\n\tcolor: #F57933;\r\n\t-webkit-transition: all 0.5s ease 0s;\r\n\ttransition: all 0.5s ease 0s;\r\n}\r\n.html5-icon:hover{\r\n\tcolor: #212529;\r\n}\r\n.css3-icon{\r\n\tcolor: #018ED5;\r\n\t-webkit-transition: all 0.5s ease 0s;\r\n\ttransition: all 0.5s ease 0s;\r\n}\r\n.css3-icon:hover{\r\n\tcolor: #212529;\r\n}\r\n.arrow-down-icon{\r\n\tpadding-top: 1em;\r\n\tfont-size: 3em;\r\n  position:relative;\r\n  -webkit-animation:bounce 1s infinite;\r\n}\r\n.prior-experience{\r\n\tpadding-top: 2em;\r\n}\r\n@-webkit-keyframes bounce {\r\n  0%       { bottom:5px; }\r\n  25%, 75% { bottom:15px; }\r\n  50%      { bottom:20px; }\r\n  100%     {bottom:0;}\r\n}\r\n/*END Icon styling*/\r\n/*Jumbotron background styling*/\r\n.jumbotron {\r\nbackground-image: url(\"/assets/Homepage/jumbotron-home-background-texture.png\");\r\nbackground-color: white;\r\nbackground-size: cover;\r\nborder-radius: 0px;\r\n}\r\n/*END Jumbotron backgrund styling*/\r\n*{\r\n\tfont-family: Maison Neue,Helvetica Neue,Helvetica,Arial,sans-serif;\r\n}\r\n\r\n", ""]);
 
 // exports
 
@@ -142,7 +145,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/components/jumbotron/jumbotron.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"jumbotron\">\n\n\t<div class=\"container\">\n\t\t\t<div class=\"col\">\n\t\t\t  <h1 class=\"display-4 font-weight-bold\">Hello my name is Zachary Toney!</h1>\n\t\t\t  <p class=\"lead tagline-icons font-weight-bold\">I built this personal website using:</p>\n\t\t\t  \n\t\t\t\t<div style=\"font-size:2em;\" class=\"icon-div\">\n\t\t\t\t\t<i class=\"fab fa-angular angular-icon\"></i>\n\t\t\t\t</div>\n\n\t\t\t\t<div style=\"font-size:2em;\" class=\"icon-div\">\n\t\t\t\t\t<i class=\"fab fa-html5 html5-icon\"></i>\n\t\t\t\t</div>\n\n\t\t\t\t<div style=\"font-size:2em;\" class=\"icon-div\">\n\t\t\t\t\t<i class=\"fab fa-css3-alt css3-icon\"></i>\n\t\t\t\t</div>\n\t\t\t</div>\n\n\t\t\t<div class=\"col embed-responsive embed-responsive-16by9\">\n\t\t\t\t<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/zN0b3kl-36k?rel=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>\n\t\t\t</div>\n\n\t\t\t<a href=\"#projects\"><div class=\"col arrow-down-icon\">\n\t\t\t\t<i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i>\n\t\t\t</div>\n\t\t</a>\n\n\t</div> <!--END container div-->\n\n\t\n</div>\n"
+module.exports = "<div class=\"jumbotron\">\n\n\t<div class=\"container\">\n\t\t\t<div class=\"col\">\n\t\t\t  <h1 class=\"display-4 font-weight-bold text-white\">Hello my name is Zachary Toney!</h1>\n\t\t\t  <p class=\"lead tagline-icons font-weight-bold text-white\">I'm a software developer. I built this personal website using:</p>\n\t\t\t  \n\t\t\t\t<div style=\"font-size:2em;\" class=\"icon-div\">\n\t\t\t\t\t<i class=\"fab fa-angular angular-icon\"></i>\n\t\t\t\t</div>\n\n\t\t\t\t<div style=\"font-size:2em;\" class=\"icon-div\">\n\t\t\t\t\t<i class=\"fab fa-html5 html5-icon\"></i>\n\t\t\t\t</div>\n\n\t\t\t\t<div style=\"font-size:2em;\" class=\"icon-div\">\n\t\t\t\t\t<i class=\"fab fa-css3-alt css3-icon\"></i>\n\t\t\t\t</div>\n\t\t\t</div>\n\n\t\t\t<div class=\"col embed-responsive embed-responsive-16by9\">\n\t\t\t\t<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/zN0b3kl-36k?rel=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>\n\t\t\t</div>\n\n\t\t<div class=\"row\">\n\t\t\t<a href=\"#skills\">\n\t\t\t\t<div class=\"col arrow-down-icon\">\n\t\t\t\t\t<i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i>\n\t\t\t\t</div>\n\t\t\t</a>\n\n\t\t\t<div class=\"col text-right prior-experience\">\n\t\t\t\t<a href=\"../../assets/Resume/ZacharyToneyResume.pdf\" target=\"_blank\"><button type=\"button\" class=\"btn btn-primary btn-lg\">Resume/Cv</button></a>\n\t\t\t</div>\n\t\t</div>\n\n\t</div> <!--END container div-->\n\n\t\n</div>\n"
 
 /***/ }),
 
@@ -203,7 +206,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/components/navbar/navbar.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n\t\n\t<a class=\"navbar-brand\" href=\"#\">\n    <img src=\"../assets/Logo/Logo.png\" width=\"30\" height=\"30\" class=\"d-inline-block align-top\" alt=\"\">\n    Zachary Toney\n  </a>\n\n \n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavDropdown\" aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n    <span class=\"navbar-toggler-icon\"></span>\n  </button>\n  <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">\n    <ul class=\"navbar-nav ml-auto\">\n      <li class=\"nav-item active\">\n        <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"#\">Features</a>\n      </li>\n      <li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"#\">Pricing</a>\n      </li>\n      <li class=\"nav-item dropdown\">\n        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n          Dropdown link\n        </a>\n        <div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownMenuLink\">\n          <a class=\"dropdown-item\" href=\"#\">Action</a>\n          <a class=\"dropdown-item\" href=\"#\">Another action</a>\n          <a class=\"dropdown-item\" href=\"#\">Something else here</a>\n        </div>\n      </li>\n    </ul>\n  </div>\n</nav>"
+module.exports = "<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n\t\n\t<a class=\"navbar-brand\" href=\"#\">\n    <img src=\"../assets/Logo/Logo.png\" width=\"30\" height=\"30\" class=\"d-inline-block align-top\" alt=\"\">\n    Zachary Toney\n  </a>\n\n \n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavDropdown\" aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n    <span class=\"navbar-toggler-icon\"></span>\n  </button>\n  <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">\n    <ul class=\"navbar-nav ml-auto\">\n      <li class=\"nav-item active\">\n        <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\n      </li>\n    </ul>\n  </div>\n</nav>"
 
 /***/ }),
 
@@ -251,7 +254,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ".first-row{\r\n\tpadding-top: 1.5em;\r\n}", ""]);
+exports.push([module.i, "", ""]);
 
 // exports
 
@@ -264,7 +267,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/components/projects-home/projects-home.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div id=\"projects\">\n\t\n\n\n\n\t<div class=\"container\">\n\n\t\t<div class=\"col text-center\">\n\t\t\t<h3>Skills</h3>\n\t\t\t<p>I realize remote working isn't the easiest. Here's a couple things I excel at.</p>\n\t\t</div>\n\t\t\n\t\t<div class=\"row first-row\">\n\n\t\t\t\n\t\t\t\t<div class=\"col\">\n\t\t\t\t\t<h4>Communication</h4>\n\t\t\t\t\t<p>I realize how imperative it is to have fantastic communication. I use tools like Slack, Skype, Duo, and a myriad of others to make sure we're both on the same page.</p>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col\">\n\t\t\t\t\t<h4>Task Tracking</h4>\n\t\t\t\t\t<p>I value my clients' precious time. That's why my goal is to work as efficiently as possible and deliver them the best possible product and service. I've used apps like Basecamp and other task management software.</p>\n\t\t\t\t</div>\n\n\n\t\t</div>\t\n\t\n\t</div>\n\n\n\n</div>"
+module.exports = ""
 
 /***/ }),
 
@@ -298,6 +301,67 @@ var ProjectsHomeComponent = /** @class */ (function () {
         __metadata("design:paramtypes", [])
     ], ProjectsHomeComponent);
     return ProjectsHomeComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/skills-home/skills-home.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".first-row{\r\n\tpadding-top: 1.5em;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/skills-home/skills-home.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div id=\"skills\">\n\n\t<div class=\"container\">\n\n\t\t<div class=\"col text-center\">\n\t\t\t<h3>Skills</h3>\n\t\t\t<p>I realize remote working isn't the easiest. Here's a couple things I excel at.</p>\n\t\t</div>\n\t\t\n\t\t<div class=\"row first-row\">\n\n\t\t\t\n\t\t\t\t<div class=\"col\">\n\t\t\t\t\t<h4>Communication</h4>\n\t\t\t\t\t<p>I realize how imperative it is to have fantastic communication. I use tools like Slack, Skype, Duo, and a myriad of others to make sure we're both on the same page.</p>\n\t\t\t\t</div>\t\t\t\t\n\t\t\t\t<div class=\"col\">\n\t\t\t\t\t<h4>Specificity</h4>\n\t\t\t\t\t<p>I think it's important to identify exactly what the client wants so it can be easily targeted and focused on to meet deadlines and quotas.</p>\n\t\t\t\t</div>\n\t\t\t\t\n\n\n\t\t</div>\n\n\t\t<div class=\"row first-row\">\n\n\t\t\t\n\t\t\t\t<div class=\"col\">\n\t\t\t\t\t<h4>Accountability</h4>\n\t\t\t\t\t<p>If something is messing up or creating errors everyone on the team has to know what happened so it can be avoided next time. That's why I love using Github for version control and most especially using GitKraken to visually see where an error may have occurred and revert back till it's fixed.</p>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col\">\n\t\t\t\t\t<h4>Task Tracking</h4>\n\t\t\t\t\t<p>I value my clients' precious time. That's why my goal is to work as efficiently as possible and deliver them the best possible product and service. I've used apps like Basecamp and other task management software.</p>\n\t\t\t\t</div>\n\n\n\t\t</div>\t\t\n\t\n\t</div>\n\n\n\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/skills-home/skills-home.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SkillsHomeComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var SkillsHomeComponent = /** @class */ (function () {
+    function SkillsHomeComponent() {
+    }
+    SkillsHomeComponent.prototype.ngOnInit = function () {
+    };
+    SkillsHomeComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-skills-home',
+            template: __webpack_require__("../../../../../src/app/components/skills-home/skills-home.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/components/skills-home/skills-home.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], SkillsHomeComponent);
+    return SkillsHomeComponent;
 }());
 
 
